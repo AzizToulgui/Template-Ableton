3069f4b4b6322294de1ad9b3bda0240a5a4058ac
Aziz <aziztoulgui20@gmail.com>
2025-03-20 18:34:26 +0100

    Start adding responsive UI

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,65 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import { FaFacebook, FaXTwitter, FaYoutube, FaInstagram, FaTiktok, FaDiscord } from "react-icons/fa6";
 
 const Footer = () => {
     return (
-        <div className="">
+        <div className="w-[85%] m-auto p-4 flex-wrap-">
             {/*Top*/}
-            <div className="flex items-center justify-around">
+            <div className="flex items-center justify-between">
                 {/*Left*/}
-                <div className="">Left</div>
+                <div className="flex flex-col gap-16">
+                    <div className="">
+                        <h1 className="text-5xl font-bold ">Ableton</h1>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-5xl font-bold ">
+                            <Link href="" className="flex gap-2 font-semibold text-lg">
+                                <p>Register Live, Push or Move</p>
+                                <span>{'>'}</span>
+                            </Link>
+                            <Link href="" className="flex gap-2 font-semibold text-lg">
+                                <p>About Ableton</p>
+                                <span>{'>'}</span>
+                            </Link>
+                            <Link href="" className="flex gap-2 font-semibold text-lg">
+                                <p>Jobs</p>
+                                <span>{'>'}</span>
+                            </Link>
+                            <div className="flex space-x-4 pt-4">
+                                <FaFacebook size={30} color="#1877F2" />
+                                <FaXTwitter size={30} />
+                                <FaYoutube size={30} color="red" />
+                                <FaInstagram size={30} color="purple" />
+                                <FaTiktok size={30} color="black" />
+                                <FaDiscord size={30} color="#5865F2" />
+                            </div>
+                        </h1>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <h1 className="font-bold text-lg">Community</h1>
+                        <Link href="" className="flex gap-2 font-semibold text-lg">
+                            <p>Find Ableton User Groups</p>
+                            <span>{'>'}</span>
+                        </Link>
+                        <Link href="" className="flex gap-2 font-semibold text-lg">
+                            <p>Find Certified Training</p>
+                            <span>{'>'}</span>
+                        </Link>
+                        <Link href="" className="flex gap-2 font-semibold text-lg">
+                            <p>Become a Certified Trainer</p>
+                            <span>{'>'}</span>
+                        </Link>
+                    </div>
+                </div>
                 {/*Middle*/}
-                <div className="">Middle</div>
+                <div className="bg-red-600">Middle</div>
                 {/*Right*/}
-                <div className="">Right</div>
+                <div className="bg-blue-600">Right</div>
             </div>
             {/*Bottom*/}
-            <div className="flex justify-between p-4">
+            <div className="flex justify-between px-0 pt-16">
                 <div className="flex items-center justify-center gap-4">
                     <Link href="/" className="text-sm font-semibold">Contact Us</Link>
                     <Link href="/" className="text-sm font-semibold">Press Resources</Link>
